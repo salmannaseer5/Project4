8ecd53128e92abeb626a9274cfcf0c2d64814f95
MJ Sandoval <salssedan@gmail.com>
2019-01-02 22:20:15 -0500

    weather

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {weatherapp as weatherapp} from './app.json';
 
 AppRegistry.registerComponent('weatherapp', () => App);
