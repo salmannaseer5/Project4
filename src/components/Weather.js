4d1bbbea7ee1dee1dd06c3f172a50dcf1354fdc2
MJ Sandoval <salssedan@gmail.com>
2019-01-04 00:21:28 -0500

    set state

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,19 +4,42 @@ import axios from axios;
 
 class Weather extends Component {
     state = {
-        city: "Bucuresti",
-        country: "Romania",
-        weatherType: "Clear",
+        city: "",
+        country: "",
+        weatherType: "",
         temperature: 21,
-        searchedCity: "Bucuresti",
+        searchedCity: "",
         val: new Animated.Value(0),
         currentColor: "rgba(255,255,255,0.5)",
         nextColor: this._randomColor(),
         icon: weatherIcon()
     };
 
+    getInitialState() {
+        return {
+          city: "Washington",
+          country: "United States",
+          weatherType: "Clear",
+          temperature: 45,
+          searchedCity: "Washington",
+          val: new Animated.Value(0),
+          currentColor: "rgba(255,255,255,0.5)",
+          nextColor: this._randomColor(),
+          icon: weatherIcon()
+        };
+      }
+
     componentWillMount(){
-        axios.get('http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric').then(response => console.log(response));
+        axios.get('http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric')
+        .then(response => this.setState({
+        temperature: weatherList.main.temp,
+        city: weatherList.name,
+        country: weatherList.sys.country,
+        weatherType: weatherList.weather[0].main,
+        currentColor: current,
+        nextColor: this._randomColor(),
+        icon: weatherIcon(weatherList.weather[0].icon)
+        }));
     }
     render () {
         return (
